c752f0684e0eaa70a959911768d0a98cc9ad94ba
jouwithcoffee <sosaciccolajoshua@gmail.com>
2024-09-08 11:45:08 -0300

    mind reader

---
+++ script.js
@@ -0,0 +1,6 @@
+let mente = prompt("Dime lo que piensas y trataré de adivinarlo: ");
+if (mente == null){
+    alert('Poné algo, no seas pajero. Ahora reiniciá la página.');
+} else {
+    alert(`Mmmm déjame pensar... Estás pensando en ${mente}`)
+}
